62f5725ca4bbbb06be1e43d85ac86c816f06f892
munchie945 <manc@rose-hulman.edu>
2014-10-29 06:21:46 -0400

    Made string processing its own function

--- src/Main.py
+++ src/Main.py
@@ -14,13 +14,16 @@ def getFiles(fileNames):
     for fileName in fileNames:
         currFile = open('../corpus/' + fileName, 'r')
         fileString = currFile.read()
-        fileString = fileString.lower()
-        fileString = fileString.replace('\n', '')
-        fileString = fileString.translate(None, string.punctuation)
-        fileString = fileString.split(' ')
-        corpus.append(fileString)
+        fileWordArray = convertToWords(fileString)
+        corpus.append(fileWordArray)
     return corpus
 
+def convertToWords(rawString):
+    rawString = rawString.lower()
+    rawString = rawString.replace('\n', '')
+    rawString = rawString.translate(None, string.punctuation)
+    return rawString.split(' ')
+    
 def printResults(scores, fileNames):
     maxResults = 10
     for i in range(maxResults):
