d8c9ced0bd28377784091eb9b4ef97415083822d
munchie945 <manc@rose-hulman.edu>
2014-10-28 21:31:50 -0400

    Added corpus in their own folder (the text files); added code to read in all of these files

--- src/Main.py
+++ src/Main.py
@@ -4,5 +4,21 @@ Created on Oct 28, 2014
 @author: manc
 '''
 
+import os
+import string
+
+def getFiles():
+    listOfFileNames = os.listdir('../corpus')
+    corpus = []
+    for fileName in listOfFileNames:
+        currFile = open('../corpus/' + fileName, 'r')
+        fileString = currFile.read()
+        fileString = fileString.lower()
+        fileString = fileString.replace('\n', '')
+        fileString = fileString.translate(None, string.punctuation)
+        fileString = fileString.split(' ')
+        corpus.append(fileString)
+    return corpus
+
 if __name__ == '__main__':
-    print 'this is the main class'
+    corpus = getFiles()
