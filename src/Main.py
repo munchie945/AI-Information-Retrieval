1029773b0176971377ee29cb1fcebbf0b76a90c5
munchie945 <manc@rose-hulman.edu>
2014-10-29 04:25:20 -0400

    Added document ranking functionality and simple result output; need to clean up more and add documentation

--- src/Main.py
+++ src/Main.py
@@ -6,11 +6,12 @@ Created on Oct 28, 2014
 
 import os
 import string
+import rankers
+from rankers import nWordsScoringFunction
 
-def getFiles():
-    listOfFileNames = os.listdir('../corpus')
+def getFiles(fileNames):
     corpus = []
-    for fileName in listOfFileNames:
+    for fileName in fileNames:
         currFile = open('../corpus/' + fileName, 'r')
         fileString = currFile.read()
         fileString = fileString.lower()
@@ -20,5 +21,18 @@ def getFiles():
         corpus.append(fileString)
     return corpus
 
+def printResults(scores, fileNames):
+    maxResults = 10
+    for i in range(maxResults):
+        index, score = scores[i]
+        if (scores > 0):
+            print fileNames[index], score
+        else:
+            break
+
 if __name__ == '__main__':
-    corpus = getFiles()
+    fileNames = os.listdir('../corpus')
+    corpus = getFiles(fileNames)
+    ranker = rankers.DocumentRanker(nWordsScoringFunction, corpus)
+    scores = ranker.rankDocuments(None)
+    printResults(scores, fileNames)
---
+++ src/rankers.py
@@ -0,0 +1,45 @@
+'''
+Created on Oct 28, 2014
+
+@author: manc
+'''
+
+class DocumentRanker(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, scoringFunction, documents):
+        self.scoringFunction = scoringFunction
+        self.documents = documents
+        
+    def rankDocuments(self, query):
+        '''
+        Takes in:
+        query: items to match in the documents, as a list of strings (punctuation stripped etc.)
+        documents: a list of documents, where each document is represented as a list of strings (with punctuation etc. removed), and a query to match
+        Returns:
+        a list of index-score tuples, sorted in descending order of score
+            The tuples are in the form (index, score), where:
+            index = index of that document in in the list of documents passed in
+            score = score of that document, from the scoring function passed in
+        '''
+        scores = []
+        for i in range(len(self.documents)):
+            currScore = self.scoringFunction(query, self.documents[i])
+            indexScoreTuple = (i, currScore)
+            scores.append(indexScoreTuple)
+        
+        # sorts the tuples in descending order based on their scores
+        scores.sort(key = lambda x: x[1], reverse = True)
+        return scores
+
+def nWordsScoringFunction(query, document):
+    '''
+    Example of a scoring function: returns a numeric score based on the document passed in (in the form of list of strings)
+    '''
+    return len(document)
+            
+        
+    
+    
